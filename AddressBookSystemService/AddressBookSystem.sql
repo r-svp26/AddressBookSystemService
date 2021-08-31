0d30dfeaf8dd020147ee48178c88b02115fb144b
r-svp26 <riteshk6341@gmail.com>
2021-09-01 04:25:26 +0530

    [Ritesh] Add: Create AddressBook table

--- AddressBookSystemService/AddressBookSystem.sql
+++ AddressBookSystemService/AddressBookSystem.sql
@@ -10,4 +10,24 @@ USE addressbook
 SELECT *
 FROM Sys.databases;
 
+/* UC2- Create Table */
+
+-- Create Table
+CREATE TABLE AddressBook
+(
+FirstName VARCHAR(15),
+LastName VARCHAR(15),
+Address VARCHAR(40),
+City VARCHAR(12),
+State VARCHAR(20),
+Zip BIGINT,
+PhoneNumber BIGINT,
+Email VARCHAR(22)
+)
+
+-- Select Statement
+SELECT *
+FROM AddressBook
+
+
 
