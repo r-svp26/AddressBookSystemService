72d1213bb208f7ebc369b9529ed5206f0ea84a7b
r-svp26 <riteshk6341@gmail.com>
2021-09-01 04:04:16 +0530

    [Ritesh] Add: Display welcome message

---
+++ AddressBookSystemService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystemService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Adress Book System Service");
+        }
+    }
+}
